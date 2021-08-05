81dfc09ee5438faf8eb0074067e0cea29f6aae67
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-08-05 13:56:38 +0700

    fetch data using hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState, useEffect, useCallback } from 'react';
 import { BiCalendar } from 'react-icons/bi';
 import Search from './Components/Search';
 import AddAppointment from './Components/AddAppointment';
 import AppointmentInfo from './Components/AppointmentInfo';
-import appointmentList from './data.json';
 
 function App() {
+  const [appointmentList, setAppointmentList] = useState([]);
+
+  // here we use useCallback() to retrieve data
+  // and monitor any changes that happen to the data
+  const fetchData = useCallback(async () => {
+    console.log('useCallback gets called');
+    await fetch('./data.json')
+      .then(response => response.json())
+      .then(data => setAppointmentList(data));
+  }, []);
+
+  // to track the fetching of data
+  // so if the data changes for some reason,
+  // useEffect() will keep track of it and update our app automatically
+  useEffect(() => {
+    console.log('useEffect gets called');
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
